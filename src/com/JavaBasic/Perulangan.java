94fd6f34c3311a4fdc487e8d091cca94b10318a0
Syaeful Anwar <35286244+syaefulanwar08@users.noreply.github.com>
2021-11-24 10:21:08 +0700

    Contoh_perulangan

---
+++ src/com/JavaBasic/Perulangan.java
@@ -0,0 +1,19 @@
+package com.JavaBasic;
+
+public class Perulangan {
+	
+	public static void main(String[] args) {
+		
+		int n = 8;
+		
+		for(int i=1+1; i<=10; i++) {
+			
+			System.out.println("i");
+			
+			// System.out.printf("%d x %d = %d%n", n, i, n*i);
+			
+		}
+		
+	}
+	
+}
